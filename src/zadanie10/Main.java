5fa15c7b5a1ff291a458b414196d3149ab9b0cac
mariuszbielanczuk <93043034+mariuszbielanczuk@users.noreply.github.com>
2021-11-06 10:09:33 +0100

    Add files via upload

--- src/zadanie10/Main.java
+++ src/zadanie10/Main.java
@@ -6,9 +6,9 @@ public class Main {
         double[] prices = {1.99, 5.99, 12.99, 49.99, 199.99};
         double totalPrice = 0.0;
         double totalPriceDicounted = 0.0;
-        for (double price : prices) {
-            totalPrice += price;
-            totalPriceDicounted += priceAfterDiscount(price, 10);
+        for (int i = 0; i < prices.length; i++) {
+            totalPrice += prices[i];
+            totalPriceDicounted += priceAfterDiscount(prices[i], i*10);
         }
         System.out.printf("Total price = %f PLN%n",totalPrice);
         System.out.printf("Total price after discount %s = %s PLN",10,totalPriceDicounted);
